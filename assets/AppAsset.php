124f3c8fcdecbb14bc0cb488313dbc5eb7c68e25
unknown <492145768@ukr.net>
2023-08-10 13:25:45 +0300

    Added new layout template

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,11 +21,13 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+
     ];
     public $js = [
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
+        'yii\bootstrap5\BootstrapAsset',
+        'yiister\gentelella\assets\Asset',
     ];
 }
